320b00f34c285657e3f95b31a495a7a252b5d498
Matt Miller <matt.f.miller@live.com>
2018-06-15 10:27:44 -0700

    Pass tests for getters and setters for instrumentType.

--- src/test/java/models/InstrumentTypeTest.java
+++ src/test/java/models/InstrumentTypeTest.java
@@ -17,14 +17,22 @@ public class InstrumentTypeTest {
     }
 
     @Test
-    public void setId() {
+    public void setId_1() {
+        InstrumentType testInstrumentType = setupNewInstrumentType();
+        testInstrumentType.setId(1);
+        assertEquals(1, testInstrumentType.getId());
     }
 
     @Test
-    public void getName() {
+    public void getName_guitar() {
+        InstrumentType testInstrumentType = setupNewInstrumentType();
+        assertEquals("Guitar", testInstrumentType.getName());
     }
 
     @Test
-    public void setName() {
+    public void setName_amp() {
+        InstrumentType testInstrumentType = setupNewInstrumentType();
+        testInstrumentType.setName("Amp");
+        assertEquals("Amp", testInstrumentType.getName());
     }
 }
