f1af42316d396357c3e2fea1e234765d374e8e31
Matt Miller <matt.f.miller@live.com>
2018-06-15 14:51:01 -0700

    Add method signatures to InstrumentDao and GuitarDao interfaces.

--- src/main/java/dao/GuitarDao.java
+++ src/main/java/dao/GuitarDao.java
@@ -1,4 +1,25 @@
 package dao;
 
+import models.Guitar;
+
+import java.util.HashMap;
+import java.util.List;
+
 public interface GuitarDao {
+
+    //CREATE
+    void add(Guitar guitar);
+
+    //READ
+    List<Guitar> getAll();
+    List<Guitar> getAllGuitarsByGuitarTypeId();
+
+    //UPDATE
+    void update(int id, HashMap<String, Object> updatedContent);
+
+    //DELETE
+    void deleteById(int id);
+
+
+
 }
--- src/main/java/dao/InstrumentDao.java
+++ src/main/java/dao/InstrumentDao.java
@@ -1,4 +1,14 @@
 package dao;
 
+import models.Instrument;
+
+import java.util.List;
+
 public interface InstrumentDao {
+    //READ
+    List<Instrument> getAll();
+    Instrument findById(int id);
+    List<Instrument> getAllInstrumentsByInstrumentTypeId();
+
+
 }
