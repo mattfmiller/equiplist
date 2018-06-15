7eedcc634e476c0c74f8169498d9b27797a290a9
Matt Miller <matt.f.miller@live.com>
2018-06-15 16:30:06 -0700

    Add post route to add instrumentType.

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,3 +1,30 @@
+import com.google.gson.Gson;
+import dao.Sql2oInstrumentTypeDao;
+import models.InstrumentType;
+import org.sql2o.Connection;
+import org.sql2o.Sql2o;
+
+
+import static spark.Spark.*;
+
 public class App {
+    public static void main(String[] args) {
+        Sql2oInstrumentTypeDao instrumentTypeDao;
+        Connection conn;
+        Gson gson = new Gson();
+
+        String connectionString = "jdbc:h2:~/equiplist.db;INIT=RUNSCRIPT from 'classpath:db/create.sql'";
+        Sql2o sql2o = new Sql2o(connectionString, "", "");
+
+        instrumentTypeDao =new Sql2oInstrumentTypeDao(sql2o);
+        conn = sql2o.open();
+
+        post("/instrumentTypes/new", "application/json", (req, res) -> {
+            InstrumentType instrumentType = gson.fromJson(req.body(), InstrumentType.class);
+            instrumentTypeDao.add(instrumentType);
+            res.status(201);
+            return gson.toJson(instrumentType);
+        });
+    }
 
 }
