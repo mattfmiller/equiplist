fe81ea36db17d93b70479592ae9a38a808bab254
Matt Miller <matt.f.miller@live.com>
2018-06-15 13:49:48 -0700

    Pass tests for getters and setters.

--- src/test/java/models/NoteTest.java
+++ src/test/java/models/NoteTest.java
@@ -17,22 +17,35 @@ public class NoteTest {
     }
 
     @Test
-    public void setId() {
+    public void setId_1() {
+        Note testNote = setupNewNote();
+        testNote.setId(1);
+        assertEquals(1, testNote.getId());
     }
 
     @Test
-    public void getContent() {
+    public void getContent_addedCustomNeckPlate() {
+        Note testNote = setupNewNote();
+        assertEquals("Added custom neck plate.", testNote.getContent());
     }
 
     @Test
     public void setContent() {
+        Note testNote = setupNewNote();
+        testNote.setContent("Added bridge cover.");
+        assertEquals("Added bridge cover.", testNote.getContent());
     }
 
     @Test
     public void getInstrumentId() {
+        Note testNote = setupNewNote();
+        assertEquals(1, testNote.getInstrumentId());
     }
 
     @Test
     public void setInstrumentId() {
+        Note testNote = setupNewNote();
+        testNote.setInstrumentId(2);
+        assertEquals(2, testNote.getInstrumentId());
     }
 }
